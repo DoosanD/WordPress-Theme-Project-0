b5c38063b91c2422d34431956ed09ca5190ef233
DoosanD <davicdusan@gmail.com>
2023-07-09 19:16:55 +0200

    09-07-2023

--- functions.php
+++ functions.php
@@ -1,15 +1,20 @@
 <?php
 
+// Load Stylesheet
 function load_css()
 {
     wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), 'v5.3.0', 'all');
 
     wp_enqueue_style('bootstrap');
+
+    wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
+
+    wp_enqueue_style('main');
 }
 
 add_action('wp_enqueue_scripts', 'load_css');
 
-
+// Load JavaScript
 function load_js()
 {
     wp_enqueue_script('jquery');
@@ -20,3 +25,16 @@ function load_js()
 }
 
 add_action('wp_enqueue_scripts', 'load_js');
+
+
+// Theme Options
+add_theme_support('menus');
+
+// Menus
+register_nav_menus(
+    array(
+        'top-menu' => 'Top Menu Location',
+        'mobile-menu' => 'Mobile Menu Location',
+        'footer-menu' => 'Footer Menu Location',
+    )
+);
