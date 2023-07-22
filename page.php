23af333a3b5f1c170a666417c64801e77e49603e
DoosanD <davicdusan@gmail.com>
2023-07-22 21:33:42 +0200

    22-07-2023

--- page.php
+++ page.php
@@ -3,6 +3,10 @@
 <section class="page-wrap">
     <div class="container">
 
+        <?php if (is_active_sidebar('page-sidebar')) : ?>
+            <?php dynamic_sidebar('page-sidebar'); ?>
+        <?php endif; ?>
+
         <h1><?php the_title(); ?></h1>
         <?php if (has_post_thumbnail()) : ?>
             <img src="<?php the_post_thumbnail_url('blog-large'); ?>" alt="<?php the_title(); ?>" class="img-fluid mb-3 img-thumbnail" />
