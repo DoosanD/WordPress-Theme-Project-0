8d7f1657685663de504a9612a60a1b3d574c15bb
DoosanD <davicdusan@gmail.com>
2023-08-05 18:48:55 +0200

    05-08-2023

--- functions.php
+++ functions.php
@@ -7,6 +7,10 @@ function load_css()
 
     wp_enqueue_style('bootstrap');
 
+    wp_register_style('magnific', get_template_directory_uri() . '/css/magnific-popup.css', array(), false, 'all');
+
+    wp_enqueue_style('magnific');
+
     wp_register_style('main', get_template_directory_uri() . '/css/main.css', array(), false, 'all');
 
     wp_enqueue_style('main');
@@ -22,6 +26,14 @@ function load_js()
     wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', 'jquery', 'v5.3.0', true);
 
     wp_enqueue_script('bootstrap');
+
+    wp_register_script('magnific', get_template_directory_uri() . '/js/jquery.magnific-popup.min.js', 'jquery', 'v5.3.0', true);
+
+    wp_enqueue_script('magnific');
+
+    wp_register_script('custom', get_template_directory_uri() . '/js/custom.js', 'jquery', 'v5.3.0', true);
+
+    wp_enqueue_script('custom');
 }
 
 add_action('wp_enqueue_scripts', 'load_js');
@@ -77,7 +89,7 @@ function my_first_post_type()
         'public' => true,
         'menu_icon' => 'dashicons-images-alt2',
         'has_archive' => true,
-        'supports' => array('title', 'editor', 'thumbnail'),
+        'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
         //'rewrite' => array('slug'=> 'cars'),
     );
     register_post_type('cars', $args);
--- single-cars.php
+++ single-cars.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
-
+<?php
+$image1 = '/wp-content/uploads/2023/08/profile.jpg';
+$image2 = '/wp-content/uploads/2023/08/profile_pic-scaled.jpg';
+?>
 <section class="page-wrap">
     <div class="container">
 
@@ -15,7 +18,26 @@
                 (<!--nextpage-->);
             </div>
             <div class="col-lg-6">
-                <?php comments_template(); ?>
+                <ul>
+                    <!-- <li>Color:<//?= get_post_meta($post->ID, 'Color', true); ?></li> -->
+                    <li>Color: <?php the_field('color'); ?></li>
+                    <li>Registration: <?php the_field('registration'); ?></li>
+                </ul>
+                <?php if (get_post_meta($post->ID, 'Registration', true)) : ?>
+                    <ul>
+                        <!-- <li>Registration:<? //= get_post_meta($post->ID, 'Registration', true); 
+                                                ?></li> -->
+                    </ul>
+                <?php endif; ?>
+                <!-- doesnt work maginfic slide, maybe because there is no repeater? -->
+                <div class="gallery">
+                    <a href="<?php echo $image1['sizes']['blog-large']; ?>">
+                        <img src="<?php echo $image1['sizes']['blog-small']; ?>" alt="ja">
+                    </a>
+                    <a href="<?php echo $image2['sizes']['blog-large']; ?>">
+                        <img src="<?php echo $image2['sizes']['blog-small']; ?>" alt="ja">
+                    </a>
+                </div>
             </div>
         </div>
 
